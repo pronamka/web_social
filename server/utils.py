eafcc6882137b219bb666e6d5f6c02eb2acf0b98
pronamka <defender0508@gmail.com>
2022-11-12 13:50:36 +0700

    modified the process of uploading files, so they are received as a body of a POST js fetch request instead of form added a feature to upload files in html format and converting them to pdf

--- server/utils.py
+++ server/utils.py
@@ -2,14 +2,18 @@ from datetime import datetime
 from time import sleep
 import pythoncom
 import os
+from typing import Union
 
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
 from PIL import Image
 from docx2pdf import convert as convert_to_pdf
+from pdfkit import from_file, configuration
 
-from server.database import DataBase
+from server.database import DataBase, DataBaseInteractError
+from server.search_engine import check_integrity
 
-allowed_extensions = ('txt', 'jpg', 'docx', 'jpeg', 'png', 'pdf', 'xlsx', 'jpeg')
+allowed_extensions = ('txt', 'jpg', 'docx', 'jpeg', 'png', 'pdf', 'xlsx', 'jpeg', 'html')
 
 
 class ExtensionNotAllowed(PermissionError):
@@ -32,51 +36,115 @@ class SymbolsNotAllowed(OSError):
 
 
 class FileManager:
-    upload_path = 'static/upload_folder/'
+    """Class for converting/saving docx/pdf files.
+    :param file: FileStorage: a docx/pdf file received from js request.
+    :param user_id: an int - id of a post's author"""
 
-    def __init__(self, file, user_id: int) -> None:
-        self.file = file
+    upload_path = 'static/upload_folder/'
+    wkhtmltopdf_config = configuration(wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
+    wkhtmltopdf_options = {'enable-local-file-access': True}
+
+    def __init__(self, file: FileStorage, user_id: int) -> None:
+        self.errors = []
+        self.file = file  # the file is of type FileStorage, not ImmutableMultiDict
         self.user_id = user_id
         self.filename = self._security_check()
-        self.protocols = {'pdf': self._immediate_save, 'docx': self._save_as_pdf}
+        self.full_path = None
+        self.protocols = {'pdf': self._immediate_save, 'docx': self._save_from,
+                          'html': self._save_from}
 
-    def save(self):
-        date = datetime.now()
-        DataBase(access_level=2).create(f'INSERT INTO posts(user_id, title, date, display_date) '
-                                        f'VALUES(?, ?, ?, ?);',
-                                        data=(self.user_id,
-                                              self.filename,
-                                              date.strftime('%Y-%m-%d'),
-                                              date.strftime("%A, %d. %B %Y %H:%M")))
-        # noinspection PyArgumentList
-        self.protocols.get(self._get_extension())(self.file)
+    def save(self) -> Union[str, None]:
+        """General function for saving files(and converting them into
+        pdf if necessary). Also checks file integrity and uniqueness of
+        its name.
+        :returns: str, if an error occurred; None if file was successfully
+        saved and an entry made in database."""
+        if conditions := self._check_conditions():
+            return conditions
+        self.full_path = os.path.join(self.upload_path, self.filename)
+        self.protocols.get(self._get_extension())()
+        if error := self._insert_into_db():
+            return error
+
+    def _insert_into_db(self) -> str:
+        """Creates an entry in the database with the
+        information about new post."""
+        try:
+            date = datetime.now()
+            DataBase(access_level=2).create(f'INSERT INTO posts(user_id, title, date, display_date) '
+                                            f'VALUES(?, ?, ?, ?);',
+                                            data=(self.user_id,
+                                                  self._new_filename(full=False),
+                                                  date.strftime('%Y-%m-%d'),
+                                                  date.strftime("%A, %d. %B %Y %H:%M")))
+        except DataBaseInteractError:
+            os.remove(self.full_path)
+            return 'A post with this name already exists, so your file was not saved.' \
+                   'Please give it a different name.'
+
+    def _check_conditions(self) -> Union[str, None]:
+        """Check if there is anything wrong with
+        file's name, and it's integrity.
+        :returns: str, if there is an error(note that it
+                  will only return a message about the latest
+                  error occurred); None, if no errors occurred."""
+        if self.errors:
+            return self.errors.pop().__repr__()
 
     def _get_extension(self) -> str:
+        """Get the extension of the file given.
+        :returns: str with the file extension (without '.')"""
         return self.filename.rsplit('.', 1)[1]
 
-    def _immediate_save(self, file):
-        file.save(os.path.join(self.upload_path, self.filename))
+    def _immediate_save(self) -> str:
+        """Just save a file (called when the file already has the right extension)."""
+        if self._get_extension() == 'pdf' and check_integrity(self.file.stream) is False:
+            #  self.file.stream is of type SpooledTemporaryFile
+            return 'Sorry, we were unable to save your file. ' \
+                   'This is likely because it was corrupted. ' \
+                   'If it opens fine on your device, please contact ' \
+                   'our tech support at defender0508@gmail.com'
+        self.file.save(self.full_path)
 
-    def _save_as_pdf(self, file):
-        file.save(os.path.join(self.upload_path, self.filename))
-        self._convert_and_save()
-        os.remove(f'static/upload_folder/{self.filename}')
+    def _save_from(self):
+        _actions = {'docx': self._convert_from_docx, 'html': self._convert_from_html}
+        self.file.save(self.full_path)
+        _actions.get(self._get_extension())()
+        os.remove(self.full_path)
 
-    def _convert_and_save(self):
+    def _convert_from_html(self):
+        """Convert file from html to pdf and save it on disk."""
+        from_file(self.full_path, self._new_filename(),
+                  options=self.wkhtmltopdf_options, configuration=self.wkhtmltopdf_config)
+
+    def _convert_from_docx(self) -> None:
+        """Convert file from docx to pdf and save it on disk."""
         pythoncom.CoInitializeEx(0)
         convert_to_pdf(os.path.join(self.upload_path, self.filename),
-                       os.path.join(self.upload_path, str(self.filename.rsplit('.', 1)[0] + '.pdf')))
+                       os.path.join(self.upload_path, self._new_filename()))
 
-    def _security_check(self) -> str:
+    def _security_check(self) -> Union[str, None]:
+        """Check if the file has allowed extension and
+        only numbers and english letters in its name.
+        :returns: str, if the filename is fine; None, if
+        there is something wrong with it. (that way an
+        error will be appended to the error log.)"""
         filename = self.file.filename
         chars = list(map(ord, filename))
         for i in chars:
-            if not(45 <= i <= 57 or 65 <= i <= 122):
-                raise SymbolsNotAllowed(filename, chr(i))
-        if filename.rsplit('.', 1)[1] == 'docx':
+            if not (45 <= i <= 57 or 65 <= i <= 122):
+                self.errors.append(SymbolsNotAllowed(filename, chr(i)))
+        if filename.rsplit('.', 1)[1] in ('docx', 'pdf', 'html'):
             return secure_filename(filename)
         else:
-            raise ExtensionNotAllowed
+            self.errors.append(ExtensionNotAllowed(filename))
+
+    def _new_filename(self, full: bool = True) -> str:
+        if not full:
+            name = self.filename
+        else:
+            name = self.full_path
+        return name.split('.', maxsplit=1)[0]+'.pdf'
 
 
 def convert_image(name: str) -> None:
@@ -87,12 +155,11 @@ def convert_image(name: str) -> None:
         image = Image.open(path)  # create an image object
         jpeg = image.convert('RGB')  # convert image into RGB representation
         os.remove(f'../static/avatar_images/{name}')  # delete the old image
-        jpeg.save(f'../static/avatar_images/{name.split(".")[0]}.jpeg')  # save the the representation as jpeg
+        jpeg.save(f'../static/avatar_images/{name.split(".")[0]}.jpeg')  # save the representation as jpeg
 
 
 class UsersObserver:
     """Class for tracking users' conditions."""
-    database = DataBase(access_level=4)
 
     def __init__(self):
         super().__init__()
@@ -108,10 +175,11 @@ class UsersObserver:
         current_time = datetime.now().strftime('%H:%M')
         for i in self._get_users():
             if self._manage_time(i[1]) < current_time:
-                self.database.delete(f"DELETE FROM users WHERE id='{i[0]}'")
+                DataBase(access_level=4).delete(f"DELETE FROM users WHERE id='{i[0]}'")
 
-    def _get_users(self):
-        users = self.database.get_all("SELECT id, registration_date FROM users WHERE status='0'")
+    @staticmethod
+    def _get_users():
+        users = DataBase().get_all("SELECT id, registration_date FROM users WHERE status='0'")
         return users
 
     @staticmethod
