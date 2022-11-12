b374e4d0a2cf1d5cbf75fbc9078681f9dbe7a220
pronamka <defender0508@gmail.com>
2022-11-12 13:39:54 +0700

    added a feature to load comments made on the author's latest posts dynamically

--- server/managers.py
+++ server/managers.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from ast import literal_eval
 from abc import ABC
-from typing import Optional
+from typing import Optional, Literal
 
 from server.database import DataBase
-from server.posts import Post, UserPostRegistry, FullyFeaturedPost
+from server.posts import Post, UserPostRegistry, FullyFeaturedPost, Comment
 
 
 class Manager(ABC):
@@ -18,6 +18,7 @@ class BasicManager(ABC):
 
 class UserManager(Manager):
     """Manager for working with user properties"""
+
     def __init__(self, user_id: int, access_level: int) -> None:
         super().__init__(access_level)
         self.user_id = user_id
@@ -25,6 +26,7 @@ class UserManager(Manager):
 
 class PostManager(Manager):
     """Manager for creating or deleting posts"""
+
     def create_post(self, user_id: int, post_title: str) -> None:
         query = "INSERT INTO posts (user_id, title, date) VALUES (?, ?, ?);"
         data_package = (user_id, post_title, datetime.now().strftime("%A, %d. %B %Y %I:%M%p"))
@@ -36,38 +38,81 @@ class PostManager(Manager):
 
 class UserPostManager(UserManager):
     """Registry containing all post of a specific user"""
+    # it's now made so this class has too many
+    # responsibilities: both over posts and comments.
+    # An effective way of separating them without
+    # violating DRY should be found
+    comments_request = 'SELECT comment_id FROM comments WHERE post_id IN {} ' \
+                       'ORDER BY comment_id DESC LIMIT {} OFFSET {}'
+
     def __init__(self, user_id: int, *args) -> None:
         super(UserPostManager, self).__init__(user_id, 4)
         self.posts = {}
+        self.post_ids = []
+        self.comments = {}
         self.post_manger = PostManager(4)
         self.count = 0
         self.post_amount = 0
+        self.comments_amount = 0
         self.fetch_posts(5)
 
-    def fetch_posts(self, amount: int) -> None:
+    def fetch_posts(self, amount: int) -> int:
         for post in UserPostRegistry.get_posts(amount, len(self.posts),
                                                self.user_id, FullyFeaturedPost):
+            self.post_ids.append(post.get_id)
             self.posts[self.post_amount] = post
             self.post_amount += 1
+        return self.post_amount
 
-    def get_latest_posts(self, amount: int, start_with: int) -> tuple:
-        """Get available_posts defined amount of user's posts starting from the latest one."""
-        if available_posts := self._check_post_sufficiency(amount + start_with - 1, amount):
-            posts = tuple(self.posts.values())[start_with:available_posts+start_with]
-            return posts
+    def fetch_comments(self, amount):
+        def get_comment_ids(comments_required):
+            ids = tuple(self.post_ids) if len(self.post_ids) != 1 else '('+str(self.post_ids[0])+')'
+            formatted_req = self.comments_request.format(ids, comments_required, self.comments_amount)
+            comments = self.database.get_many_singles(formatted_req, size=comments_required)
+            if len(comments) < comments_required:
+                st_length = self.post_amount
+                self.fetch_posts(2)
+                if self.post_amount - st_length != 0:
+                    comments += get_comment_ids(comments_required - len(comments))
+            return comments
 
-    def _check_post_sufficiency(self, length: int, amount: int) -> Optional[int]:
+        for i in get_comment_ids(amount):
+            self.comments[self.comments_amount] = Comment(i)
+            self.comments_amount += 1
+        return self.comments_amount
+
+    def get_latest(self, amount: int, start_with: int,
+                   subject: Literal['posts', 'comments'] = 'posts') -> tuple:
+        """Get available_posts defined amount of user's posts starting from the latest one.
+        :param amount: the amount of required objects.
+        :param start_with: an integer, that defines, how many results should be
+                           ignored (starting from the beginning of any registry)
+        :param subject: a string, either 'posts' or 'comments' - defines
+                        what information is required.
+        """
+        protocols = {'posts': self.posts, 'comments': self.comments}
+        if available := self._check_sufficiency(amount, start_with, subject):
+            result = tuple(protocols.get(subject).values())[start_with:available + start_with]
+            return result
+
+    def _check_sufficiency(self, required_amount: int, start_with: int, subject: str) -> Optional[int]:
         """Check if there is enough posts to return.
         If not, attempt will be made to load some new ones.
         If there is still not enough to return even one None will be returned."""
-        if length > self.post_amount:
-            self.fetch_posts(length - self.post_amount)
-        if (available_posts := self.post_amount-amount) <= 0 and length > self.post_amount:
+        protocols = {'posts': (self.fetch_posts, self.post_amount),
+                     'comments': (self.fetch_comments, self.comments_amount)}
+        length = required_amount + start_with
+        func, amount = protocols.get(subject)
+        if length > amount:
+            amount = func(length - amount)
+        if (subjects_available := (amount - required_amount)) <= 0 and start_with > amount:
             return
-        elif length > self.post_amount and available_posts > 0:
-            return available_posts
+        elif start_with < amount and subjects_available <= 0:
+            return amount - start_with
+        elif length > amount and subjects_available > 0:
+            return subjects_available
         else:
-            return amount
+            return required_amount
 
     def get_post_amount(self):
         posts = self.database.get_information(f'SELECT COUNT(DISTINCT post_id) FROM posts '
@@ -79,19 +124,10 @@ class UserPostManager(UserManager):
                                                  f'WHERE post_id="{post_id}"')
         return comments
 
-    """def delete_post(self, post_id: int) -> None:
-        assert self.posts.get(post_id) is not None, \
-            f"No post with given id belongs to user: user id: {self.user_id}, requested post id: {post_id}"
-        self.post_manger.delete_post(post_id)
-        self.posts.pop(post_id)"""
-
     def get_post(self, post_id: int) -> Post:
         assert self.posts.get(post_id) is not None, \
             f"No post with given id belongs to user: user id: {self.user_id}, requested post id: {post_id}"
         return self.posts.get(post_id)
-
-    """def create_post(self, post_title: str) -> None:
-        self.post_manger.create_post(self.user_id, post_title)"""
 
     @property
     def get_posts(self) -> dict:
@@ -106,7 +142,7 @@ class UserPostManager(UserManager):
             raise StopIteration
         else:
             self.count += 1
-            return list(self.posts.values())[self.count-1]
+            return list(self.posts.values())[self.count - 1]
 
     def __repr__(self):
         return f"{self.__class__.__name__}(user_id={self.user_id})"
@@ -114,6 +150,7 @@ class UserPostManager(UserManager):
 
 class UserSubscriptionManager(UserManager):
     """Manager for controlling subscriptions"""
+
     def __init__(self, user_id: int) -> None:
         super().__init__(user_id, 3)
         self.followers, self.follows = list(map(literal_eval, self._get_followers()))
@@ -129,7 +166,7 @@ class UserSubscriptionManager(UserManager):
 
     def add_follower(self, user_id: int) -> None:
         assert user_id not in self.followers, "Some user tried to follow you twice. " \
-                                        "If you encountered this error, please contact " \
+                                              "If you encountered this error, please contact " \
                                               "us at defender0508@gmeail.com"
         self.followers.append(user_id)
         self.database.update(f"UPDATE users SET followers='{self.followers}' WHERE id='{self.user_id}'")
@@ -148,11 +185,8 @@ class UserSubscriptionManager(UserManager):
 
     def check_if_subscribed(self, user_id: int) -> None:
         assert user_id not in self.follows, "You tried to follow the person you are already following." \
-                                        "If you encountered this error, " \
+                                            "If you encountered this error, " \
                                             "please contact us at defender0508@gmeail.com"
-
-    def get_ffollowers(self):
-        return self.followers
 
     @property
     def get_followers(self):
