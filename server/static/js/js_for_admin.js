8403b58719b32818d54651495c419b49a99d791c
pronamka <defender0508@gmail.com>
2022-11-19 13:58:50 +0700

    changed the system of loading posts

--- server/static/js/js_for_admin.js
+++ server/static/js/js_for_admin.js
@@ -1,4 +1,4 @@
-const request_url = '/admin_get_posts'
+const request_url = '/admin_get_posts/'
 var posts = {}
 var extension_amount = 0
 
@@ -19,12 +19,15 @@ function getPosts(amount){
     sendRequest('POST', request_url, 
     {'posts_loaded': extension_amount, 'posts_required': amount}).then(resp => {
         post_json = JSON.parse(resp)
+        console.log(extension_amount)
         extension_amount += amount
+        console.log(extension_amount)
         buildPosts(post_json);
     })
 }
 function buildPosts(post_dict){
-    for (var i=0, l=Object.keys(post_dict).length; i<=l; i++){
+    console.log(post_dict)
+    for (var i=0, l=Object.keys(post_dict).length-1; i<=l; i++){
         try{
             title = post_dict[i]['title'];
             author = post_dict[i]['author'];
@@ -45,27 +48,27 @@ function buildPosts(post_dict){
     }
 }
 function buildHTML(path, author, title, post_id, creation_date){
-    html = `<div class="table_cell" id="`+post_id+`">
-            <iframe width="400" height="500" src="`+ path+`"></iframe>
-            <button onclick="verify_post(`+post_id+`);">Verify</button>
-            <button onclick="window.location.href='/admin/examine_post/?post_id=`+ post_id+`'">Examine</button>
+    html = `<div class="table_cell" id="${post_id}">
+            <iframe width="400" height="500" src="${path}"></iframe>
+            <button onclick="verify_post(${post_id});">Verify</button>
+            <button onclick="window.location.href='/admin/examine_post/?post_id=${post_id}'" 
+            id="examine_post_btn ${post_id}">Examine</button>
             <details class="post_data" open>
                 <summary>Post information</summary>
                 <ul>
-                <li>Title: `+title+`</li>
-                <li>Author: `+author+`</li>
-                <li>Post id: `+post_id+`</li>
-                <li>Created on:`+creation_date+`</li>
+                <li>Title: ${title}</li>
+                <li>Author: ${author}</li>
+                <li>Post id: ${post_id}</li>
+                <li>Created on:${creation_date}</li>
                 </ul>
             </details>
         </div>`;
     return html
 }
 function verify_post(post_id){
-    sendRequest('POST', '/admin/verify_post/', post_id).then(resp => {
+    sendRequest('POST', '/admin/verify_post/', {'post_id': post_id}).then(resp => {
         const verified_post = document.getElementById(post_id)
         verified_post.remove()
-        extension_amount -= 1
         getPosts(1)
     }
     )
@@ -80,11 +83,8 @@ function addPost(html) {
     tab.appendChild(template.content);
     return template.content.firstChild;
 }
-current_position = 250
 window.addEventListener("scroll", function(event) {
-    var top = this.scrollY
-    if (current_position < top){
-        current_position += 1000
+    if ((window.innerHeight+window.scrollY) >= document.body.offsetHeight-100){
         getPosts(2);
     } 
 }, false);
