b9a6b6d79b12e43906e6b7df23b07301939e84fc
pronamka <defender0508@gmail.com>
2022-11-21 19:03:26 +0700

    simplified the process of checking for unconfirmed users

--- server/utils.py
+++ server/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep
 from docx2pdf import convert
 from pythoncom import CoInitializeEx
@@ -166,21 +166,12 @@ class UsersObserver:
             sleep(3600)
 
     def _check_on_loop(self):
-        current_time = datetime.now().strftime('%H:%M')
+        current_time = datetime.now()
         for i in self._get_users():
-            if self._manage_time(i[1]) < current_time:
+            if datetime.strptime(i[1], '%A, %d. %B %Y %H:%M') < current_time - timedelta(hours=1):
                 self.database.delete(f"DELETE FROM users WHERE id='{i[0]}'")
 
     @classmethod
     def _get_users(cls):
         users = cls.database.get_all("SELECT id, registration_date FROM users WHERE status='0'")
         return users
-
-    @staticmethod
-    def _manage_time(time: str) -> str:
-        comparison_time = time.rsplit(' ', maxsplit=1)[1]
-        if int(comparison_time.split(':')[0]) >= 23:
-            comparison_time = '00' + ':' + comparison_time.split(':')[1]
-        else:
-            comparison_time = str(int(comparison_time.split(':')[0]) + 1) + ':' + comparison_time.split(':')[1]
-        return comparison_time
