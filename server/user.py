f8802ed0b6ea5964319538b7425592e55bd8786e
pronamka <defender0508@gmail.com>
2022-12-10 15:50:06 +0700

    added methods to like and view posts to user class

--- server/user.py
+++ server/user.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from enum import Enum
 from abc import ABC, abstractmethod
@@ -8,7 +9,7 @@ from flask_login import UserMixin
 from flask_mail import Mail, Message
 
 from server.managers import UserSubscriptionManager, UserPostManager
-from server.posts import Comment
+from server.posts import FullyFeaturedPost, Comment
 from server.database import DataBase
 
 
@@ -16,7 +17,7 @@ class UserRole(Enum):
     AnonymousUser = -1
     EmailNotConfirmed = 0
     Viewer = 1
-    Creator = 2
+    Author = 2
     Admin = 3
 
 
@@ -30,24 +31,51 @@ class User(UserMixin, ABC):
         self.SubscriptionManager = UserSubscriptionManager(self.user_id)
         self.avatar = self._get_avatar()
 
-    def update_interests(self, interests):
+    def update_interests(self, interests: dict) -> None:
         DataBase(access_level=3).update(f'UPDATE users SET interests="{interests}" WHERE id={self.user_id}')
         self.interests = interests
 
-    def _get_avatar(self):
-        return '/'+path if os.path.exists((path := f'static/avatar_images/{self.user_id}.jpeg')) \
+    def view_post(self, post_id: int):
+        query = 'UPDATE posts SET views=CASE WHEN EXISTS ' \
+                '   (SELECT user_id FROM post_views WHERE user_id={user_id} AND post_id={post_id})' \
+                '       THEN (SELECT views FROM posts WHERE post_id={post_id}) ' \
+                '   ELSE ((SELECT views FROM posts WHERE post_id={post_id})+1) END WHERE post_id={post_id}; ' \
+                'INSERT OR IGNORE INTO post_views(user_id, post_id, created_on) VALUES ({user_id}, {post_id}, "{time}")'
+        DataBase(access_level=4).execute_script(query.format(user_id=self.user_id,
+                                                             post_id=post_id, time=self._get_time()))
+
+    def change_like_status(self, post_id: int, new_status: int = 1) -> None:
+        if new_status == '1':
+            query = 'INSERT INTO post_likes(user_id, post_id, created_on) VALUES (?, ?, ?)'
+            DataBase(access_level=2).insert(query, data=(self.user_id, post_id, self._get_time()))
+        elif new_status == '0':
+            DataBase(access_level=4).delete(f'DELETE FROM post_likes '
+                                            f'WHERE user_id={self.user_id} AND post_id={post_id}')
+
+    @staticmethod
+    def _get_time():
+        return datetime.datetime.now().strftime('%Y-%m-%d')
+
+    def check_if_liked(self, post_id):
+        query = f'SELECT user_id FROM post_likes WHERE ' \
+                f'user_id={self.user_id} AND post_id={post_id}'
+        res = DataBase(access_level=1).get_information(query, default=(0, ))
+        return res
+
+    def _get_avatar(self) -> str:
+        return '/' + path if os.path.exists((path := f'static/avatar_images/{self.user_id}.jpeg')) \
             else '/static/avatar_images/0.jpeg'
 
     @property
-    def get_user_id(self):
+    def get_user_id(self) -> int:
         return self.user_id
 
     @property
-    def get_login(self):
+    def get_login(self) -> str:
         return self.login
 
     @property
-    def get_email(self):
+    def get_email(self) -> str:
         return self.email_address
 
     @property
@@ -56,27 +84,27 @@ class User(UserMixin, ABC):
         """Get a role of the user."""
 
     @property
-    def get_avatar(self):
+    def get_avatar(self) -> str:
         return self.avatar
 
     @property
-    def is_authenticated(self):
+    def is_authenticated(self) -> bool:
         if self.status <= 0:
             return False
         else:
             return True
 
     @property
-    def get_interests(self):
+    def get_interests(self) -> dict:
         return self.interests
 
-    def get_id(self):
+    def get_id(self) -> str:
         try:
             return str(self.user_id)
         except AttributeError:
             raise NotImplementedError("No `id` attribute - override `get_id`") from None
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         parameters_string = ''
         for attr in self.__dict__.items():
             parameters_string += f"{attr[0]} = {attr[1]}, \n\t"
@@ -85,54 +113,92 @@ class User(UserMixin, ABC):
 
 
 class Viewer(User):
+    """User that hasn't made any posts."""
+
     def __init__(self, data: tuple) -> None:
         super().__init__(data)
 
     @property
-    def get_role(self):
+    def get_role(self) -> UserRole.Viewer:
         return UserRole.Viewer
 
 
 class Author(User):
+    """User that uploaded at least once, and
+    his article was verified by admins."""
+
     reply_sql = f'INSERT INTO comments(post_id, user_id, comment, date, is_reply, status) ' \
                 f'VALUES(?, ?, ?, ?, ?, ?);'
 
-    def __init__(self, data: tuple):
+    def __init__(self, data: tuple) -> None:
         super().__init__(data)
         self.PostManager = UserPostManager(self.user_id)
 
+    def send_notification(self, post_id: int, app) -> None:
+        """Send notification about the new publication made to all of user's
+        subscribers."""
+        mail = Mail(app)
+        article = FullyFeaturedPost(post_id)
+        tags_info = f' about {", ".join(tags)}' if (tags := article.get_tags(True)) else ''
+        message = Message(f'{self.login} released new article.',
+                          recipients=self._get_followers_emails())
+        message.html = f'{self.login} released an article "{article.get_title}"{tags_info}'
+        mail.send(message)
+
+    def _get_followers_emails(self) -> list:
+        """Get email of all user's subscribers."""
+        followers_ids = tuple(fol) if len(fol := self.SubscriptionManager.get_followers) \
+            != 1 else f'({fol[0]})'  # sqlite break if the expression `SELECT ... IN `
+        # used with the tuple with a single element inside it (e.g. with the
+        # tuple that looks something like this: `(1,)`), so it needs to be converted to a string
+        # like `(1)`.
+        emails = DataBase().get_all_singles(f'SELECT email FROM users WHERE id IN {followers_ids}')
+        return emails
+
     def author_reply(self, post_id: int, reply_text: str,
                      accurate_time: str, comment_id: int) -> None:
+        """Reply to a comment. This method should be specifically used when the
+        author replies to a comment under his posts, because it gives the comment a
+        status that indicates that the comment is helpful/interesting."""
         database = DataBase(access_level=3)
         data_package = (post_id, self.user_id, reply_text, accurate_time, comment_id, 1)
         database.insert(self.reply_sql, data=data_package)
         database.update(f'UPDATE comments SET status=2 WHERE comment_id="{comment_id}"')
 
-    def author_ban_comment(self, current_app, comment_id: int, reason: Optional[str] = None):
+    def author_ban_comment(self, current_app, comment_id: int, reason: Optional[str] = None) -> None:
+        """Bun a comment under the user's post."""
         CommentBanMessageSender(current_app, comment_id,
                                 self.get_login, reason).send_ban_notification()
 
     @property
-    def get_role(self):
-        return UserRole.Creator
+    def get_role(self) -> UserRole.Author:
+        """Get the role of the user."""
+        return UserRole.Author
 
 
 class Admin(User):
-    def __init__(self, data: tuple):
+    """User with special rights to ban or verify posts."""
+
+    def __init__(self, data: tuple) -> None:
         super().__init__(data)
         self.PostManager = UserPostManager(self.user_id)
 
     @classmethod
-    def verify_post(cls, post_id: int) -> None:
+    def verify_post(cls, post_id: int, app) -> None:
+        """Set the post's status to `verified`, make other users able to see it."""
         db = DataBase(access_level=3)
         db.update(f'UPDATE posts SET verified=1 WHERE post_id={post_id}')
+        article_author = UserFactory.create_author(db.get_information(f'SELECT user_id FROM posts'
+                                                                      f' WHERE post_id={post_id}')[0])
+        article_author.send_notification(post_id, app)
 
     def ban_post(self, post_id: int, problem_description: str, current_app) -> None:
+        """Ban a post, email the author to inform him that post was banned."""
         PostBanMessageSender(current_app, post_id, problem_description,
                              self.get_email).send_ban_notification()
 
     @property
-    def get_role(self):
+    def get_role(self) -> UserRole.Admin:
         return UserRole.Admin
 
 
@@ -258,7 +324,7 @@ class UserFactory:
         """Automatically decides, which role user should
         have by looking it up in the database."""
         _actions = {UserRole.Viewer: cls.create_viewer,
-                    UserRole.Creator: cls.create_author,
+                    UserRole.Author: cls.create_author,
                     UserRole.Admin: cls.create_admin}
         # noinspection PyArgumentList
         return _actions.get(cls.get_role(user_id))(user_id)
