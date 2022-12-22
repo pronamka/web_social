e72b2f2d789cce0462c5ca7eefdf7c45f17e7934
pronamka <defender0508@gmail.com>
2022-12-22 09:52:15 +0300

    added functions to handle content dynamic loading when scrolling

--- server/static/js/js_for_all.js
+++ server/static/js/js_for_all.js
@@ -1,3 +1,52 @@
+// function now() and throttle() (that used here for 
+// sending request properly (to avoid sending to many at)
+// once) are taken from https://github.com/jashkenas/underscore.git
+
+function now() {
+    return new Date().getTime();
+};
+
+function throttle(func, wait, options) {
+    var timeout, context, args, result;
+    var previous = 0;
+    if (!options) options = {};
+  
+    var later = function() {
+      previous = options.leading === false ? 0 : now();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    };
+  
+    var throttled = function() {
+      var _now = now();
+      if (!previous && options.leading === false) previous = _now;
+      var remaining = wait - (_now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = _now;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  
+    throttled.cancel = function() {
+      clearTimeout(timeout);
+      previous = 0;
+      timeout = context = args = null;
+    };
+  
+    return throttled;
+  }
+
 document.addEventListener('DOMContentLoaded', loadAvatar())
 function sendRequest(method, url, body) {
     const headers = {'Content-Type': 'application/json'}
@@ -11,6 +60,7 @@ function sendRequest(method, url, body) {
 }
 function loadAvatar(){
     sendRequest('POST', '/load_info/', {'page': 'personal_data'}).then(resp =>{
+    console.log(resp)
         console.log(resp['avatar'])
         document.getElementById('user_avatar_pic_img').innerHTML = `<img src="${JSON.parse(resp)['avatar']}" class="avatar"></img>`
     })
@@ -30,12 +80,10 @@ function search(){
 }
 
 
-`current_position = 250
-window.addEventListener("scroll", function(event) {
-    var top = this.scrollY
-    console.log(top, current_position)
-    if (current_position < top){
-        current_position += 1000
-        getPosts();
-    } 
-}, false);`
+var load_on_scroll = function(event) {
+    
+    if ((window.innerHeight+window.scrollY) >= document.body.offsetHeight-200){
+        console.log('called')
+        getPosts(2);
+    } }
+document.addEventListener("scroll", throttle(load_on_scroll, 100));
