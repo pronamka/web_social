c95cbc0480dd636804d19fb0213eba2f2b6f3fd9
pronamka <defender0508@gmail.com>
2022-12-22 10:03:26 +0300

    modified the functions for displaying posts to include additional info (how much time ago the post was made, authors avatar)

--- server/static/js/scripts_for_subscription_page.js
+++ server/static/js/scripts_for_subscription_page.js
@@ -1,7 +1,7 @@
 const request_url = '/load_info/';
 
 var settings = {'page': 'subscription_page', 'posts_required': 5, 'posts_loaded': 0,
-'last_date': 0}
+'last_date': 0, 'post_type': 2}
 
 document.addEventListener('DOMContentLoaded', getPosts());
 
@@ -22,21 +22,32 @@ function getPosts(){
         buildPosts(posts_dict['dated_posts']);
     })
 }
+
+function buildTagsRepresentation(tags){
+    var repr = ''
+    for (i = 0; i<tags.length; i++){
+        repr += `<li>${tags[i]}</li>`
+    }
+    return repr
+}
+
 function buildPosts(posts){
-    console.log(posts)
     if (posts.length > 0){
-        for (var i=0, l=Object.keys(posts).length; i<=l; i++){
+        for (var i=0, l=Object.keys(posts).length; i<l; i++){
             try{
                 if (settings['last_date'] != posts[i]['creation_date']){
-                    addHTML(`<h3>${posts[i]['creation_date']}</h3>`)
+                    addHTML(`<h3 class="date-date-separator">${posts[i]['creation_date']}</h3>`)
                     addHTML('<div class="ln-dates-sep"></div>')
                     settings['last_date'] = posts[i]['creation_date']
                 }
                 title = posts[i]['title'];
                 author = posts[i]['author'];
                 post_id = posts[i]['post_id'];
+                author_avatar = posts[i]['author_avatar']
+                date = posts[i]['made_ago_str']
+                tags = buildTagsRepresentation(posts[i]['tags_flattened'])
                 path = '/static/upload_folder/'+title;
-                post = buildHTML(path, author, title, post_id);
+                post = buildHTML(path, author, title, post_id, date, author_avatar, tags);
                 addHTML(post);
             }
             catch(error){
@@ -45,14 +56,30 @@ function buildPosts(posts){
         }
     }
 }
-function buildHTML(path, author, title, post_id){
+
+function buildHTML(path, author, title, post_id, date, author_avatar, tags){
     html = `<object class="table_cell">
-            <iframe width="250" height="400" class="preview" src="`+ path+`"></iframe>
-            <div class="for_preview">
-                <p class="post_name">
-                <a href="/view_post/?post_id=`+post_id+`">`+ title+`</a>
-                </p><br><p class="person_name">by `+ author+`</p>
-            </div>
+                <iframe class="preview" src="${path}"></iframe>
+                <div class='all-post-info'>
+                    <img class="author-avatar" src="/static/${author_avatar}">
+                    <div class="post-short-information">
+                        <div class="title-and-author">
+                            <p class="post-name">
+                                <a href="/view_post/?post_id=${post_id}">${title}</a>
+                            </p>
+                            <p class="person_name">${author}</p>
+                        </div>
+                        <p class="made-ago">${date}</p>
+                    </div>
+                    <div class='post-tags'>
+                        <details>
+                            <summary>Post tags</summary>
+                            <ul>
+                                ${tags}
+                            </ul>
+                        </details>
+                    </div>
+                </div>
             </object>`
             ;
     return html
@@ -67,11 +94,3 @@ function addHTML(html) {
     return template.content.firstChild;
 }
 
-current_position = 250
-window.addEventListener("scroll", function(event) {
-    var top = this.scrollY
-    if (current_position < top){
-        current_position += 1000
-        getPosts();
-    } 
-}, false);
