243a08673424b4c21ab0ef19c5200c38524f3941
pronamka <defender0508@gmail.com>
2023-02-17 20:31:48 +0700

    added functions for marking the entire science tree as chosen when choosing interests

--- server/static/js/personal_page.js
+++ server/static/js/personal_page.js
@@ -1,151 +1,180 @@
-const url = '/load_info/'
-var user_current_interests = ''
-var interests_plain = {}
-var sciences_descriptions = {}
-const science_description_field = document.getElementById('science-description')
-document.addEventListener('DOMContentLoaded', loadPersonalData())
+
 function sendAvatar(){
     const file_data = document.getElementById('avatar_file').files[0]
     const file = new FormData()
     file.append('file', file_data)
     file.append('filename', file_data[name])
     fetch('/change_avatar/', {method: 'POST', body: file, headers: {'Accept': 'appliction/json'}}).then(resp => {
-        alert(JSON.parse(resp), window.location.reload())
+        if (resp.status === 200){
+            alert('Avatar Changed', window.location.reload())
+        }
+        else{
+            alert('Something went wrong!')
+        }
     })
 }
-function sendRequest(method, url, body) {
-    const headers = {'Content-Type': 'application/json'}
-    return fetch(url, {
-        method: method,
-        body: JSON.stringify(body),
-        headers: headers}).then(response => {
-        return response.text()
-    })
-}
+
+
 function loadPersonalData(){
     sendRequest('POST', url, {'page': 'personal_data'}).then(resp => {
         user_current_interests = JSON.parse(resp)['interests']
+        article_tags_manager.tags = user_current_interests
+        article_tags_manager.tags_plain = flatten_dict(user_current_interests)
         var avatar = document.getElementById('avatar')
-        console.log(resp)
         avatar.innerHTML = `<img src=${JSON.parse(resp)['avatar']}>`
-        console.log(avatar)
-        insertHTML(displayInterests(user_current_interests))
+        insertHTML('#interests_list', displayInterests(user_current_interests))
     })
 }
+
+
 function sendInterestsChanges(){
-    sendRequest('POST', '/change_interests/', {'interests': user_current_interests}).then(resp => {
+    sendRequest('POST', '/change_interests/', {'interests': article_tags_manager.tags}).then(resp => {
         window.location.reload()
     })
 }
 
-function getParent(key){
-    console.log(key)
+
+function getParent(key, action=false){
     var elem = document.getElementById(key)
+    if (action){
+        elem.style.backgroundColor = 'lightgreen'
+        elem.setAttribute('class', 'interests selected')
+        document.getElementById(`interest-status-mark ${key}`).innerHTML = '&#x2713;'
+    }
     var child_of = elem.getAttribute('child_of')
     return child_of
 }
-function getFullInheritanceTree(key){
-    var elem = document.getElementById(key)
-    var child_of = elem.getAttribute('child_of')
+
+
+function getFullInheritanceTree(key, action=false){
+
     var tree = {}
-    s = {}
-    s[key] = {}
-    if (child_of=="All interests"){
-        return s
-    }
-    tree[child_of] = s
+    tree[key] = {}
     while (true){
-        var parent = getParent(child_of)
-        if (parent == 'All interests'){
-            return tree
+        parent = getParent(key, action)
+        if (parent=='All interests'){
+            return tree;
         }
-        var s = tree
-        tree = {}
-        tree[parent] = s
-        child_of = parent
+        var temp = {}
+        temp[parent] = tree
+        tree = temp
+        key = parent
     }
 }
-function searchAndInsert(interest_tree, user_tree){
-    var interest = Object.keys(interest_tree)[0]
-    interests_plain[interest] = {}
-    if (interest in user_tree){
-        user_tree[interest] = searchAndInsert(interest_tree[interest], user_tree[interest])
-        return user_tree
-    }
-    else{
-        var further_interests = interest_tree[interest]
-        if (further_interests == null){
-            further_interests = {}
-        }
-        user_tree[interest] = further_interests
-        return user_tree
-    }
-}
-function flatten(interest_tree_dict){
-    var keys = Object.keys(interest_tree_dict)
+
+function flatten_dict(interest_tree_dict){
     var result = {}
-    for (var i=0, l=keys.length; i<l; i++){
-        result[keys[i]] = {}
-        if (interest_tree_dict[keys[i]].length != 0){
-            result = Object.assign({}, result, flatten(interest_tree_dict[keys[i]]))
+    for (const i in interest_tree_dict){
+        result[i] = {}
+        if (interest_tree_dict[i].length != 0){
+            result = Object.assign({}, result, flatten_dict(interest_tree_dict[i]))
         }
     }
     return result
 }
-function search(aim, tree){
-    if (aim in tree){
-        return tree[aim]
+
+
+class ArticleTagsManager{
+    constructor(){
+        this.tags_plain = {}
+        this.tags = {}
     }
-    else{
-        return search(aim, tree)
-    }
-}
-function searchAndDelete(interest_tree, user_tree){
-    var interest = Object.keys(interest_tree)[0]
-    if (Object.keys(interest_tree[interest]).length != 0){
-        user_tree[interest] = searchAndDelete(interest_tree[interest], user_tree[interest])
-        return user_tree
-    }
-    else{
-        var s = flatten(user_tree[interest])
-        for (i in s){
-            delete interests_plain[i]
-        }
-        delete user_tree[interest]
-        delete interests_plain[interest]
-        return user_tree
-    }
-}
-function changeStatus(key){
-    var details = document.getElementById(`details ${key}`)
-    if (details){
-        if (details.open){
-            details.open = false
+
+    changeTagStatus(key){
+        this.keepDetailsUntouched(key)
+        const elem = document.getElementById(key)
+        const mark = document.getElementById(`interest-status-mark ${key}`)
+        if (key in this.tags_plain){
+            //if the key is already in the plain representation of
+            //the chosen tags, then the science was selected earlier
+            //and so the user wants to unselect it.
+            const tree = getFullInheritanceTree(key)
+            this.unselectChildren(key)
+            this.tags = this.removeTag(tree, this.tags)
+            elem.setAttribute('class', 'interests unselected')
+            elem.style.backgroundColor = 'lightcoral'
+            mark.innerHTML = '&#x2717;'
         }
         else{
-            details.open = true
+            const tree = getFullInheritanceTree(key, true)
+            this.tags = this.insertTag(tree, this.tags)
+            this.tags_plain = Object.assign({}, this.tags_plain, flatten_dict(this.tags))
+            elem.setAttribute('class', 'interests selected')
+            mark.innerHTML = '&#x2713;'
         }
     }
-    elem = document.getElementById(`${key}`)
-    var tree = getFullInheritanceTree(key)
-    var mark = document.getElementById(`interests_chosen_mark ${key}`)
-    if (key in interests_plain) {
-        user_current_interests = searchAndDelete(tree, user_current_interests)
-        elem.setAttribute('class', 'interests unselected')
-        mark.innerHTML = '&#x2717;'
+
+    unselectChildren(key){
+        var parent = document.getElementById(`details ${key}`)
+        if (parent===null){
+            return 0;
+        }
+        var elems = parent.getElementsByClassName('interests selected')
+        for (var i=0; i<elems.length; i++){
+            elems[i].style.backgroundColor = 'lightcoral'
+            elems[i].getElementsByClassName('interest-status-mark')[0].innerHTML = '&#x2717;'
+        }
     }
-    else{
-        user_current_interests = searchAndInsert(tree, user_current_interests)
-        interests_plain = Object.assign({}, interests_plain, flatten(tree))
-        console.log(user_current_interests)
-        interests_plain[key] = {}
-        elem.setAttribute('class', 'interests selected')
-        mark.innerHTML = '&#x2713;'
+
+    insertTag(interest_tree, user_tree){
+        var interest = Object.keys(interest_tree)[0]
+        this.tags_plain[interest] = {}
+        if (interest in user_tree){
+            user_tree[interest] = this.insertTag(interest_tree[interest], user_tree[interest])
+            return user_tree
+        }
+        else{
+            var further_interests = interest_tree[interest]
+            if (further_interests == null){
+                further_interests = {}
+            }
+            user_tree[interest] = further_interests
+            return user_tree
+        }
+    }
+
+    removeTag(interest_tree, user_tree){
+        var interest = Object.keys(interest_tree)[0]
+        if (Object.keys(interest_tree[interest]).length != 0){
+            user_tree[interest] = this.removeTag(interest_tree[interest], user_tree[interest])
+            return user_tree
+        }
+        else{
+            var s = flatten_dict(user_tree[interest])
+            for (const i in s){
+                delete this.tags_plain[i]
+            }
+            delete user_tree[interest]
+            delete this.tags_plain[interest]
+            return user_tree
+        }
+    } 
+
+    keepDetailsUntouched(key){
+        //when the user click the mark in the summary to select/unselect
+        //a tag, details shoud not be opened or closed, so the details state
+        //is kept untouched
+        var details = document.getElementById(`details ${key}`)
+        if (details){
+            if (details.open){
+                details.open = false
+            }
+            else{
+                details.open = true
+            }
+        }
     }
 }
+
+
+function changeStatus(key){
+    article_tags_manager.changeTagStatus(key)
+}
+
+
 function getIndex(interest){
     var mark = `<p onclick="changeStatus('${interest}')" 
-    id="interests_chosen_mark ${interest}" class='interest-status-mark'>`
+    id="interest-status-mark ${interest}" class='interest-status-mark'>`
     if (interest in interests_plain){
         var index = 'class="interests selected"'
         mark += '&#x2713;'
@@ -158,17 +187,19 @@ function getIndex(interest){
     return [index, mark]
 }
 
+
 function showScienceDescription(science_name){
-    console.log(science_name)
     if (sciences_descriptions[science_name]){
         science_description_field.innerText = sciences_descriptions[science_name]
     }
 }
 
+
 function hideScienceDescription(science_name){
     science_description_field.innerText = 
     'A short description of a science will appear here when you hover over it.'
 }
+
 
 function displayInterests(interests, summary_name='Your interests', with_indexing=false, add_to_chosen=true, summary_index=['', ''], child_of=''){
     var onhover = ''
@@ -208,28 +239,25 @@ function displayInterests(interests, summary_name='Your interests', with_indexin
     html += `</ul></details>`
     return html
 }
-function insertHTML(html, parent_node_id='interests_list'){
-    const tab = document.getElementById(parent_node_id);
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    tab.appendChild(template.content);
-    return template.content.firstChild;
-}
+
 function chooseInterest(){
     loadAllInterests()
     document.getElementById('manage_interests_dlg').showModal()
 }
+
 function loadAllInterests(){
     sendRequest('POST', '/load_info/', {'page': 'personal_data', 'all_interests': true}).then(resp => {
-        console.log(resp)
         var interests_and_descriptions = JSON.parse(resp)['interests']
         html = displayInterests(interests_and_descriptions['interests'], 'All interests', true, false)
         sciences_descriptions = interests_and_descriptions['descriptions']
-        insertHTML(html, 'all_interests')
+        insertHTML('#all_interests', html)
     })    
 }
 
+
+
+
+/* Avatar dialog */
 function displayFilename(){
     var file_input = document.getElementById('avatar_file').files
     if (file_input.length == 0){
@@ -238,3 +266,20 @@ function displayFilename(){
     }
     document.getElementById('new-avatar-filename').innerText = file_input[0].name
 }
+
+
+
+
+const url = '/load_info/'
+
+var user_current_interests = {}
+
+var interests_plain = {}
+
+var sciences_descriptions = {}
+
+const article_tags_manager = new ArticleTagsManager()
+
+const science_description_field = document.getElementById('science-description')
+
+document.addEventListener('DOMContentLoaded', loadPersonalData)
